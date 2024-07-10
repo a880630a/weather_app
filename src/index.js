c17dba995549c599b754bf35003bb6b69c152bd3
Xian <dragonball880630@gmail.com>
2024-07-11 04:12:46 +0800

    feat: 設置tailwind環境

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
+import "./global.scss";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
